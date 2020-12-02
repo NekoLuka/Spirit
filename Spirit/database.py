bbc4870a664fbb7174efdfc9fa63fa8b4ca5f492
nekoluka <lukadejong2002@gmail.com>
2020-12-02 21:36:29 +0100

    Added cursor to database for efficiency and later support. Added check for autocommit. Added sqlite3 row factory for efficiency and dictionary indexing

--- Spirit/database.py
+++ Spirit/database.py
@@ -4,20 +4,19 @@ class database:
     def __init__(self, dbName: str, autoCommit: bool=False):
         self.__autoCommit = autoCommit
         self.__conn = sqlite3.connect(dbName)
+        self.__conn.row_factory = sqlite3.Row
+        self.__conn.isolation_level = None if autoCommit else "DEFERRED"
+        self.__cursor = self.__conn.cursor()
         
-    def createTable(self, name: str, **kwargs):
+    def createTable(self, name: str, **kwargs) -> bool:
         self.__conn.execute(f"CREATE TABLE IF NOT EXISTS {name} (" + ", ".join([f"{name} {value}" for name, value in kwargs.items()]) + ")")
-        if self.__autoCommit:
-            self.commit()
 
-    def insert(self, name: str, **kwargs):
+    def insert(self, name: str, **kwargs) -> bool:
         self.__conn.execute(f"INSERT INTO {name} (" + 
                             ", ".join([name for name in kwargs.keys()]) + 
                             ") VALUES (" + 
                             ", ".join(["?" for i in range(len(kwargs))]) + 
                             ")", [i for i in kwargs.values()])
-        if self.__autoCommit:
-            self.commit()
 
     def commit(self):
         self.__conn.commit()
