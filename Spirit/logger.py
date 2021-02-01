a6a9571f85ebbe24f07c0023caafcb8b478f7965
nekoluka <lukadejong2002@gmail.com>
2021-02-01 18:47:23 +0100

    updated logger

--- Spirit/logger.py
+++ Spirit/logger.py
@@ -1,26 +1,21 @@
 import time
 
 class logger:
-    DISABLED = -1
-    INFO = 0
-    CRITICAL = 1
-    ERROR = 2
-    WARNING = 3
-    DEBUG = 4
+    DISABLED = 0
+    INFO = 1
+    CRITICAL = 2
+    ERROR = 4
+    WARNING = 8
+    DEBUG = 16
 
-    __reportLevel = 2
-    __logLevel = 1
+    __reportType = 15
 
     __fileName = "Spirit.log"
     __fileObject = None
 
     @classmethod
-    def setLogLevel(cls, level: int=1):
-        cls.__logLevel = level
-
-    @classmethod
-    def setReportLevel(cls, level: int=2):
-        cls.__reportLevel = level
+    def setReportTypes(cls, type: int=15):
+        cls.__reportType = type
 
     @classmethod
     def setLogFile(cls, path: str="Spirit.log"):
@@ -29,42 +24,42 @@ class logger:
     @classmethod
     def info(cls, message: str):
         data = time.ctime() + " - INFO: " + message + "\n"
-        if cls.INFO <= cls.__reportLevel:
-            print(data)
-        if cls.INFO <= cls.__logLevel and cls.__fileObject:
-            cls.__fileObject.write(data)
+        if cls.__reportType & cls.INFO == cls.INFO:
+            print(data, end="")
+            if cls.__fileObject:
+                cls.__fileObject.write(data)
 
     @classmethod
     def critical(cls, message: str):
         data = time.ctime() + " - CRITICAL: " + message + "\n"
-        if cls.CRITICAL <= cls.__reportLevel:
-            print(data)
-        if cls.CRITICAL <= cls.__logLevel and cls.__fileObject:
-            cls.__fileObject.write(data)
+        if cls.__reportType & cls.CRITICAL == cls.CRITICAL:
+            print(data, end="")
+            if cls.__fileObject:
+                cls.__fileObject.write(data)
 
     @classmethod
     def error(cls, message: str):
         data = time.ctime() + " - ERROR: " + message + "\n"
-        if cls.ERROR <= cls.__reportLevel:
-            print(data)
-        if cls.ERROR <= cls.__logLevel and cls.__fileObject:
-            cls.__fileObject.write(data)
+        if cls.__reportType & cls.ERROR == cls.ERROR:
+            print(data, end="")
+            if cls.__fileObject:
+                cls.__fileObject.write(data)
 
     @classmethod
     def warning(cls, message: str):
         data = time.ctime() + " - WARNING: " + message + "\n"
-        if cls.WARNING <= cls.__reportLevel:
-            print(data)
-        if cls.WARNING <= cls.__logLevel and cls.__fileObject:
-            cls.__fileObject.write(data)
+        if cls.__reportType & cls.WARNING == cls.WARNING:
+            print(data, end="")
+            if cls.__fileObject:
+                cls.__fileObject.write(data)
 
     @classmethod
     def debug(cls, message: str):
         data = time.ctime() + " - DEBUG: " + message + "\n"
-        if cls.DEBUG <= cls.__reportLevel:
-            print(data)
-        if cls.DEBUG <= cls.__logLevel and cls.__fileObject:
-            cls.__fileObject.write(data)
+        if cls.__reportType & cls.DEBUG == cls.DEBUG:
+            print(data, end="")
+            if cls.__fileObject:
+                cls.__fileObject.write(data)
 
     @classmethod
     def run(cls):
